afe2f74bc499fcb3051a935b96f320415c18e59c
Sean1572 <sean.hyatt.perry@gmail.com>
2021-05-03 22:46:51 -0700

    code to handle package changes

--- audino/frontend/scripts/run-dev.sh
+++ audino/frontend/scripts/run-dev.sh
@@ -4,6 +4,12 @@ set -o errexit
 
 app="/app/frontend"
 echo "npm install"
-cd "${app}" && npm install
+{
+    cd "${app}" && npm install
+} || {
+    cd "${app}" && rm -rf node_modules
+    cd "${app}" && rm -f package-lock.json
+    cd "${app}" && npm install
+}
 echo "npm start"
 cd "${app}" && npm run start
