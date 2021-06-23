9eeb7ec2b72c342c54f2e7ac37fab6bb8a8bde07
Sean1572 <sean.hyatt.perry@gmail.com>
2021-06-23 15:30:16 -0700

    Added a fix to combine working mysql start up code with rclone setup

--- audino/mysql/start.sh
+++ audino/mysql/start.sh
@@ -1,4 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-cron;
+apt-get update && apt-get -y install cron
+
+cp ./mysql/crontab /etc/cron.d/backup-cron
+
+cp ./mysql/start.sh /
+cp ./mysql/docker-entrypoint.sh /
+
+chmod +x /start.sh
+chmod +x /start.sh
+
+chmod 0644 /etc/cron.d/backup-cron
+
+crontab /etc/cron.d/backup-cron
+
+touch /var/log/cron.log
+
+
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+bash /docker-entrypoint.sh
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 mysqld --user=root --init-file=/mysql/create_database.sql --character-set-server=utf8mb4 --collation-server=utf8mb4_bin
