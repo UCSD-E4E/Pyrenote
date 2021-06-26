8cd52a8495944c62ab84b099586a3ed00d664020
vkt <vktiwari@ucsd.edu>
2021-06-25 17:58:36 -0700

    Remove redundant custom startup script

--- audino/mysql/start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash-
-echo "You are login as: `whoami`"
-cron;
-#mysqld --user=root --init-file=/mysql/create_database.sql --character-set-server=utf8mb4 --collation-server=utf8mb4_bin
