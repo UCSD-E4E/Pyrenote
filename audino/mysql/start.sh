544a59be40b0615a2c7365941743144d295fc932
vkt <vktiwari@ucsd.edu>
2021-07-01 12:31:45 -0700

    Restore latest backup at every restart

--- audino/mysql/start.sh
+++ audino/mysql/start.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-mysqld --init-file=/mysql/create_database.sql --character-set-server=utf8mb4 --collation-server=utf8mb4_bin;
-
-if [ "${RESTORE}" == "true" ]; then
-	latest_backup=/mnt/backup/`ls /mnt/backup/ | grep -Eo "[0-9]{8}-[0-9]{2}:[0-9]{2}.sql" | sort -ru | head -n 1`;
-	mysqladmin -f -u root --password=root drop audino;
-	mysqladmin -u root --password=root create audino;
-	mysql -u root --password=root audino < $latest_backup;
-fi
+echo "Restoring latest database backup"
+latest_backup=/mnt/backup/`ls /mnt/backup/ | grep -Eo "[0-9]{8}-[0-9]{2}:[0-9]{2}.sql" | sort -ru | head -n 1`;
+mysqladmin -f -u root --password=root drop audino;
+mysqladmin -u root --password=root create audino;
+mysql -u root --password=root audino < $latest_backup;
