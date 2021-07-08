f80a37dc5bc542b65ac1f7f144dbdeb9a010e9fe
Sean1572 <sean.hyatt.perry@gmail.com>
2021-07-07 21:28:26 -0700

    Fixed label bug from #83

--- audino/backend/routes/projects.py
+++ audino/backend/routes/projects.py
@@ -337,8 +337,11 @@ def add_label_to_project(project_id):
     try:
         project = Project.query.get(project_id)
         app.logger.info(f"Label: {LabelType.query.all()} AT 0")
-        app.logger.info(f"Label: {Label.query.all()} AT 0")
-        label = Label(name=label_name, type_id=label_type_id, id=(len(Label.query.all()))+1)
+        label_id = 1
+        if len(Label.query.all()) > 0:
+            last_label = Label.query.all()[len(Label.query.all())-1]
+            label_id = last_label.id+1       
+        label = Label(name=label_name, type_id=label_type_id, id=label_id)
         project.labels.append(label)
         db.session.add(project)
         db.session.commit()
