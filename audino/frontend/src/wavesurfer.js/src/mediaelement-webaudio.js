bd2609fdc832481876f3d696f7a9805f6c04ef65
Sean1572 <sean.hyatt.perry@gmail.com>
2021-07-09 16:04:51 -0700

    Updated Wavesurfer.js Library Code

--- audino/frontend/src/wavesurfer.js/src/mediaelement-webaudio.js
+++ audino/frontend/src/wavesurfer.js/src/mediaelement-webaudio.js
@@ -10,67 +10,68 @@ import MediaElement from './mediaelement';
  * @since 3.2.0
  */
 export default class MediaElementWebAudio extends MediaElement {
-  /**
-   * Construct the backend
-   *
-   * @param {WavesurferParams} params Wavesurfer parameters
-   */
-  constructor(params) {
-    super(params);
-    /** @private */
-    this.params = params;
-    /** @private */
-    this.sourceMediaElement = null;
-  }
+    /**
+     * Construct the backend
+     *
+     * @param {WavesurferParams} params Wavesurfer parameters
+     */
+    constructor(params) {
+        super(params);
+        /** @private */
+        this.params = params;
+        /** @private */
+        this.sourceMediaElement = null;
+    }
 
-  /**
-   * Initialise the backend, called in `wavesurfer.createBackend()`
-   */
-  init() {
-    this.setPlaybackRate(this.params.audioRate);
-    this.createTimer();
-    this.createVolumeNode();
-    this.createScriptNode();
-    this.createAnalyserNode();
-  }
+    /**
+     * Initialise the backend, called in `wavesurfer.createBackend()`
+     */
+    init() {
+        this.setPlaybackRate(this.params.audioRate);
+        this.createTimer();
+        this.createVolumeNode();
+        this.createScriptNode();
+        this.createAnalyserNode();
+    }
+    /**
+     * Private method called by both `load` (from url)
+     * and `loadElt` (existing media element) methods.
+     *
+     * @param {HTMLMediaElement} media HTML5 Audio or Video element
+     * @param {number[]|Number.<Array[]>} peaks Array of peak data
+     * @param {string} preload HTML 5 preload attribute value
+     * @private
+     */
+    _load(media, peaks, preload) {
+        super._load(media, peaks, preload);
+        this.createMediaElementSource(media);
+    }
 
-  /**
-   * Private method called by both `load` (from url)
-   * and `loadElt` (existing media element) methods.
-   *
-   * @param {HTMLMediaElement} media HTML5 Audio or Video element
-   * @param {number[]|Number.<Array[]>} peaks Array of peak data
-   * @param {string} preload HTML 5 preload attribute value
-   * @private
-   */
-  _load(media, peaks, preload) {
-    super._load(media, peaks, preload);
-    this.createMediaElementSource(media);
-  }
+    /**
+     * Create MediaElementSource node
+     *
+     * @since 3.2.0
+     * @param {HTMLMediaElement} mediaElement HTML5 Audio to load
+     */
+    createMediaElementSource(mediaElement) {
+        this.sourceMediaElement = this.ac.createMediaElementSource(
+            mediaElement
+        );
+        this.sourceMediaElement.connect(this.analyser);
+    }
 
-  /**
-   * Create MediaElementSource node
-   *
-   * @since 3.2.0
-   * @param {HTMLMediaElement} mediaElement HTML5 Audio to load
-   */
-  createMediaElementSource(mediaElement) {
-    this.sourceMediaElement = this.ac.createMediaElementSource(mediaElement);
-    this.sourceMediaElement.connect(this.analyser);
-  }
+    play(start, end) {
+        this.resumeAudioContext();
+        return super.play(start, end);
+    }
 
-  play(start, end) {
-    this.resumeAudioContext();
-    return super.play(start, end);
-  }
+    /**
+     * This is called when wavesurfer is destroyed
+     *
+     */
+    destroy() {
+        super.destroy();
 
-  /**
-   * This is called when wavesurfer is destroyed
-   *
-   */
-  destroy() {
-    super.destroy();
-
-    this.destroyWebAudio();
-  }
+        this.destroyWebAudio();
+    }
 }
