9ba28acdf84a87bf9aa5938bbf7c3128c863c2c1
vkt <vktiwari@ucsd.edu>
2021-07-10 00:27:02 -0700

    Provide an option to specify a backup file to restore the database from

--- audino/mysql/start.sh
+++ audino/mysql/start.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Restoring latest database backup"
-latest_backup=/mnt/backup/`ls /mnt/backup/ | grep -Eo "[0-9]{8}-[0-9]{2}:[0-9]{2}.sql" | sort -ru | head -n 1`;
-mysqladmin -f -u root --password=root drop audino;
-mysqladmin -u root --password=root create audino;
+if [ -d /backup/db_file ]; then \
+	latest_backup=/mnt/backup/`ls /mnt/backup/ | grep -Eo "[0-9]{8}-[0-9]{2}:[0-9]{2}.sql" | sort -ru | head -n 1`; \
+f1; \
+if [ -f /backup/db_file ]; then \
+	latest_backup=/backup/db_file; \
+fi;
+
+tail $latest_backup; 
+mysqladmin -f -u root --password=root drop audino; \
+mysqladmin -u root --password=root create audino; \
 mysql -u root --password=root audino < $latest_backup;
