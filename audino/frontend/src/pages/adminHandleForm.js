d26892311ae2c11bbe098f2c2009495c7330e072
Nishant Balaji <nishantb1130@gmail.com>
2021-07-16 18:09:54 -0700

    linting

--- audino/frontend/src/pages/adminHandleForm.js
+++ audino/frontend/src/pages/adminHandleForm.js
@@ -1,9 +1,5 @@
-import axios from 'axios';
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
 import {
-  faPlusSquare,
   faEdit,
   faUserPlus,
   faTags,
@@ -12,59 +8,57 @@ import {
   faUpload
 } from '@fortawesome/free-solid-svg-icons';
 import { IconButton } from '../components/button';
-import Loader from '../components/loader';
-import FormModal from '../containers/modal';
 
 class AdminHandleFormProjects extends React.Component {
   constructor(props) {
     super(props);
-    this.adminProps = props.adminProps
-    this.project = props.project
-    this.showModal = (modalProps) => {
-      props.showModal(modalProps)
-    } 
+    this.adminProps = props.adminProps;
+    this.project = props.project;
+    this.showModal = modalProps => {
+      props.showModal(modalProps);
+    };
   }
 
   handleAddLabelsToProject(e, projectId) {
     const { history } = this.adminProps;
     history.push(`/projects/${projectId}/labels`);
-  };
+  }
 
   handleEditProject(e, projectId) {
-   this.showModal({
+    this.showModal({
       formType: 'Edit_PROJECT',
       title: 'Edit Project',
       projectId
     });
-  };
+  }
 
   handleAddUsersToProject(e, projectId, projectName) {
-   this.showModal({
+    this.showModal({
       formType: 'MANAGE_PROJECT_USERS',
       title: `Project ${projectName}: Manage User Access`,
       projectId
     });
-  };
+  }
 
-  handleUploadDataToProject(e, projectName, projectId, api_key)  {
-   this.showModal({
+  handleUploadDataToProject(e, projectName, projectId, api_key) {
+    this.showModal({
       formType: 'UPLOAD_DATA',
       title: `Project ${projectName}: Upload Project Audio Files`,
       projectId,
       projectName,
       api_key
     });
-  };
+  }
 
   handleDownloadDataToProject(e, projectName, projectId, api_key) {
-   this.showModal({
+    this.showModal({
       formType: 'DOWNLOAD_DATA',
       title: `Project ${projectName}: DOWNLOAD ANNOTATIONS`,
       projectId,
       projectName,
       api_key
     });
-  };
+  }
 
   render() {
     return (
@@ -73,21 +67,19 @@ class AdminHandleFormProjects extends React.Component {
           icon={faUserPlus}
           size="sm"
           title="Manage users"
-          onClick={e =>
-            this.handleAddUsersToProject(e,this.project.project_id,this.project.name)
-          }
+          onClick={e => this.handleAddUsersToProject(e, this.project.project_id, this.project.name)}
         />
         <IconButton
           icon={faTags}
           size="sm"
           title="Manage labels"
-          onClick={e => this.handleAddLabelsToProject(e,this.project.project_id)}
+          onClick={e => this.handleAddLabelsToProject(e, this.project.project_id)}
         />
         <IconButton
           icon={faEdit}
           size="sm"
           title="Edit Annotations"
-          onClick={e => this.handleEditProject(e,this.project.project_id)}
+          onClick={e => this.handleEditProject(e, this.project.project_id)}
         />
         <div />
         <IconButton
@@ -97,9 +89,9 @@ class AdminHandleFormProjects extends React.Component {
           onClick={e =>
             this.handleUploadDataToProject(
               e,
-             this.project.name,
-             this.project.project_id,
-             this.project.api_key
+              this.project.name,
+              this.project.project_id,
+              this.project.api_key
             )
           }
         />
@@ -110,34 +102,34 @@ class AdminHandleFormProjects extends React.Component {
           onClick={e =>
             this.handleDownloadDataToProject(
               e,
-             this.project.name,
-             this.project.project_id,
-             this.project.api_key
+              this.project.name,
+              this.project.project_id,
+              this.project.api_key
             )
           }
         />
       </td>
-    )
+    );
   }
 }
 
 class AdminHandleFormUsers extends React.Component {
   constructor(props) {
     super(props);
-    this.user = props.user
-    this.showModal = (modalProps) => {
-      props.showModal(modalProps)
-    } 
+    this.user = props.user;
+    this.showModal = modalProps => {
+      props.showModal(modalProps);
+    };
   }
 
-  handleEditUser(e, userId){
-   this.showModal({ formType: 'EDIT_USER', title: 'Edit User', userId });
-  };
+  handleEditUser(e, userId) {
+    this.showModal({ formType: 'EDIT_USER', title: 'Edit User', userId });
+  }
 
   handleDeleteUser(e, userId) {
     // TODO: CREATE MODAL TO CONFRIM BUT FOR NOW MAKE DEV BUTTON
-   this.showModal({ formType: 'DELETE_USER', title: 'Delete User', userId });
-  };
+    this.showModal({ formType: 'DELETE_USER', title: 'Delete User', userId });
+  }
 
   render() {
     return (
@@ -155,11 +147,8 @@ class AdminHandleFormUsers extends React.Component {
           onClick={e => this.handleDeleteUser(e, this.user.user_id)}
         />
       </td>
-    )
+    );
   }
-
 }
 
-
-
-export {AdminHandleFormProjects, AdminHandleFormUsers}
+export { AdminHandleFormProjects, AdminHandleFormUsers };
