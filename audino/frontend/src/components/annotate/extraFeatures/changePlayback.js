2dcc45cfcbfc6d8f36495553dd1580525a10066b
Sean1572 <sean.hyatt.perry@gmail.com>
2021-08-13 15:50:51 -0700

    Linted and added pause
    lint and when user changes the playback input, wavesurfer pauses

--- audino/frontend/src/components/annotate/extraFeatures/changePlayback.js
+++ audino/frontend/src/components/annotate/extraFeatures/changePlayback.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import StickySlider from '../../stickySlider';
+
 const ChangePlayback = props => {
   const { annotate } = props;
   const { state } = annotate;
-  const { wavesurfer, playbackOn } = state;
+  const { wavesurfer, wavesurferMethods, playbackOn } = state;
   const stickySpeeds = [0, 25, 50, 75, 100, 125, 150, 175, 200];
 
-  const changeCallback = (value) => {
+  const changeCallback = value => {
+    wavesurferMethods.handlePause()
     wavesurfer.setPlaybackRate(value / 100);
-  }
+  };
 
   return (
     <div>
@@ -18,14 +20,14 @@ const ChangePlayback = props => {
           <StickySlider
             min="0"
             max="200"
-            changeCallback={(value) => changeCallback(value)}
+            changeCallback={value => changeCallback(value)}
             stickyPos={stickySpeeds}
             threshold={5}
           />
           <br />
         </div>
       ) : null}
-      <br/>
+      <br />
     </div>
   );
 };
--- audino/frontend/src/components/stickySlider.js
+++ audino/frontend/src/components/stickySlider.js
@@ -3,84 +3,91 @@ import PropTypes from 'prop-types';
 
 const noop = () => {};
 
+const StickySlider = ({
+  min = 0,
+  max = 10,
+  hasLabels = true,
+  threshold = 10,
+  stickyPos = [],
+  changeCallback = noop
+}) => {
+  const [value, setValue] = React.useState(Math.floor((max - min) / 2));
 
-const StickySlider = ({ min = 0, max = 10, hasLabels = false, threshold = 10, stickyPos = [], changeCallback = noop }) => {
-  const [value, setValue] = React.useState(Math.floor((max - min) / 2))
-  
-  const handleChange = (e) => {
+  const handleChange = e => {
     const newValue = e.target.value;
-    changeCallback(newValue)
-    setValue(newValue)
-  }
-
-
+    changeCallback(newValue);
+    setValue(newValue);
+  };
 
   const handleStick = e => {
     let finalValue = e.target.value;
     stickyPos.forEach(pos => {
-      if (Math.abs(pos - finalValue) < 20) finalValue = pos;
+      if (Math.abs(pos - finalValue) < threshold) finalValue = pos;
     });
-    changeCallback(finalValue)
-    setValue(finalValue)
-  }
+    changeCallback(finalValue);
+    setValue(finalValue);
+  };
 
-  const computePos = (index) => {
+  const computePos = index => {
     const val = stickyPos[index];
     const newVal = Number(((val - min) * 100) / (max - min));
     return `calc(${newVal}% + (${7 - newVal * 0.16}px))`;
-  }
+  };
 
-  const computeSpacing = (index) => {
+  const computeSpacing = index => {
     const val = String(stickyPos[index]);
-    const length = val.length
-    return  length * -4 + "px"
-  }
+    const length = val.length;
+    return `${length * -4}px`;
+  };
 
   const renderLabels = () => {
-    const leftBound = 1.25
     return (
-      <div style={{position: "relative", width: "100%", height: "24px", left: "0px", textAlign: "center"}}>
-        {
-          stickyPos.map((label, index) => { 
-            return (
-              <div 
-                style={{
-                  textAlign: "center",
-                  float: "left",
-                  position: "absolute",
-                  overflow: "visible",
-                  zIndex: 1,
-                  left: computePos(index),
-                  textAlign: "left",
-                  justifyContent: "left",
-                }}
-              >
-                <div 
-                  className="vl" 
-                />
-                <text style={{position: "relative", left: computeSpacing(index), top: "-5px"}}>{label}</text>
-              </div>
-              
-            )
-          })
-        }
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '24px',
+          left: '0px',
+          textAlign: 'center'
+        }}
+      >
+        {stickyPos.map((label, index) => {
+          return (
+            <div
+              style={{
+                float: 'left',
+                position: 'absolute',
+                overflow: 'visible',
+                zIndex: 1,
+                left: computePos(index),
+                textAlign: 'left',
+                justifyContent: 'left'
+              }}
+            >
+              <div className="vl" />
+              <text style={{ position: 'relative', left: computeSpacing(index), top: '-5px' }}>
+                {label}
+              </text>
+            </div>
+          );
+        })}
       </div>
-    )
-  }
-  console.log("rerendered")
+    );
+  };
+
   return (
-    <div className="sideMenuItem" style={{overflow: "visible"}}>
-      <div style={{position: "relative", width: "100%"}}>
-        <div style={{position: "absolute", width: "100%", height: "24px", top: "-4px"}}>
-          <div style={{padding: "0px", zIndex: 90,  overflow: "visible"}} className="sideMenuItem">
-            {renderLabels()}
+    <div className="sideMenuItem" style={{ overflow: 'visible' }}>
+      <div style={{ position: 'relative', width: '100%' }}>
+        <div style={{ position: 'absolute', width: '100%', height: '24px', top: '-4px' }}>
+          <div style={{ padding: '0px', zIndex: 90, overflow: 'visible' }} className="sideMenuItem">
+            {hasLabels && renderLabels()}
           </div>
         </div>
       </div>
-      <input 
+      <input
         className="sideMenuItem"
         id="StickySlide"
-        style={{padding: "0px", zIndex: 100, position: "relative", overflow: "visible"}}
+        style={{ padding: '0px', zIndex: 100, position: 'relative', overflow: 'visible' }}
         type="range"
         min={min}
         max={max}
