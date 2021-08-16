b689f443e701640b1b4c4e7927704605088ececf
Sean Perry <sean.hyatt.perry@gmail.com>
2021-08-16 14:15:10 -0700

    Added markers for sticky range (#197)
    * start of side window rework
    
    * Added expand handler
    
    * Added easing animation package
    
    * Linted and Fixed Data Bug
    
    * Cleaned up code styling a bit
    
    * linting
    
    * Added tabs for window
    
    Next step: add with spectrogram changer controls to side menu
    
    * added wavesform icon edit
    
    * attempt at fixing custom icon
    
    * Labeled side window sections and linted
    
    * Moved extra features into reference window
    
    * Lint
    
    * Fixed apply previous annotation bug
    
    * Added final styling changes
    
    * Added sticky needs
    
    * ran lint
    
    * fixed linting error
    
    * built labels for stickySlider
    
    * Linted and added pause
    
    lint and when user changes the playback input, wavesurfer pauses

--- audino/frontend/src/components/annotate/extraFeatures/changePlayback.js
+++ audino/frontend/src/components/annotate/extraFeatures/changePlayback.js
@@ -1,41 +1,33 @@
 import React from 'react';
+import StickySlider from '../../stickySlider';
 
 const ChangePlayback = props => {
   const { annotate } = props;
   const { state } = annotate;
-  const { wavesurfer, playbackOn, playbackRate } = state;
-  const stickySpeeds = [25, 50, 75, 100, 125, 150, 175, 200];
-  const changePlayback = e => {
-    const speed = e.target.value;
-    wavesurfer.setPlaybackRate(speed / 100);
-    annotate.setState({ playbackRate: speed });
-  };
+  const { wavesurfer, wavesurferMethods, playbackOn } = state;
+  const stickySpeeds = [0, 25, 50, 75, 100, 125, 150, 175, 200];
 
-  const stickToSpeed = e => {
-    let speed = e.target.value;
-    stickySpeeds.forEach(value => {
-      if (Math.abs(speed - value) < 10) speed = value;
-    });
-    wavesurfer.setPlaybackRate(speed / 100);
-    annotate.setState({ playbackRate: speed });
+  const changeCallback = value => {
+    wavesurferMethods.handlePause()
+    wavesurfer.setPlaybackRate(value / 100);
   };
 
   return (
     <div>
       {playbackOn ? (
-        <div className="sideMenuItem">
+        <div>
           <text style={{ textAlign: 'center' }}>Change Playback</text>
-          <input
-            type="range"
-            min="1"
+          <StickySlider
+            min="0"
             max="200"
-            value={playbackRate}
-            onChange={e => changePlayback(e)}
-            onMouseUp={e => stickToSpeed(e)}
+            changeCallback={value => changeCallback(value)}
+            stickyPos={stickySpeeds}
+            threshold={5}
           />
           <br />
         </div>
       ) : null}
+      <br />
     </div>
   );
 };
