574a0271e46009b426deeefbd67594e9c10110fc
Sean Perry <sean.hyatt.perry@gmail.com>
2022-08-09 12:59:26 -0700

    Fixed issue that hid annotations from other users if the conditions where not correct (#296)

--- audino/backend/routes/current_user.py
+++ audino/backend/routes/current_user.py
@@ -61,7 +61,7 @@ def fetch_data_for_project(project_id):
         categories = ["pending", "completed", "marked_review", "all"]
         
         data = {}
-        if(project.is_iou):
+        if(project.is_iou or project.is_example):
             segmentations = db.session.query(Segmentation.data_id).distinct().filter_by(created_by=identity["username"]).subquery()
             data = retrieve_database_iou(project_id, segmentations, request_user, identity["username"])
         else:
