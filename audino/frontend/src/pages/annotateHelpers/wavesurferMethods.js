aead6043b34b33aff1f288314889c0b1dd1479a1
Sean Perry <sean.hyatt.perry@gmail.com>
2022-08-26 17:15:28 -0700

    All flies under pages have comments
    Remaining work:
    Backend
    Frontend
    - Containers
    - Components

--- audino/frontend/src/pages/annotateHelpers/wavesurferMethods.js
+++ audino/frontend/src/pages/annotateHelpers/wavesurferMethods.js
@@ -1,3 +1,8 @@
+/**
+ * This file contains the wavesurferMethods Class, a class that can
+ * interact with wavesurfer to adjust the render of the spectrogram and regions
+ */
+
 import React from 'react';
 import {
   faBackward,
@@ -16,12 +21,22 @@ const colormap = require('colormap');
 /**
  * Useful object paths:
  * wavesurfer.spectrogram.canvas
+ *  - WebCanvas for rendering the spectrogram
+ * 
  * wavesurfer.spectrogram.wrapper
+ *  - Wrapper for the spectrogram
+ * 
  * wavesurfer.drawer
+ *  - Draws objects on canvas for spectrogram and regions
+ *  
  * wavesurfer.regions.list
+ *  - List of region data that wavesurfer currently has rendered
  */
 
+
+
 class WavesurferMethods {
+  //Props come from annotate.js
   constructor(props) {
     this.state = props.state;
     this.annotate = props.annotate;
@@ -29,25 +44,44 @@ class WavesurferMethods {
     this.unsavedButton = null;
   }
 
+  /**
+   * If annotate's state changes, update the state for WavesurferMethods
+   * @param {*} state 
+   */
   updateState(state) {
     this.state = state;
   }
 
+  /**
+   * Change state of annotate from wavesurferMethods
+   * @param {*} state 
+   */
   setState(items) {
     this.annotate.setState(items);
   }
 
+  /**
+   * Initlize the canvas for spectrogram and regions without loading in audio data yet
+   * @param {*} state 
+   */
   loadWavesurfer() {
+    //Get some state values
     const boundingBox = this.boundingBox;
     const { active } = this.state;
+
+    //This is the color map used by the sprectogram
     const spectrogramColorMap = colormap({
       colormap: 'hot',
       nshades: 256,
       format: 'float'
     });
 
+    //Begin the loading process
     this.setState({ isDataLoading: true });
     const fftSamples = 512;
+
+    //Create wavesurfer with spectrogram and region plugins enabled
+    //Please refer to wavesurfer.js website for what these values are
     const wavesurfer = WaveSurfer.create({
       container: '#waveform',
       barWidth: 0,
@@ -79,25 +113,37 @@ class WavesurferMethods {
         })
       ]
     });
+
+    //Get the history react object and create a new UnsavedButton
+    //TODO: WHAT DOES THE UNSAVED BUTTON DO AGAIN?????
     const { history } = this.annotate.props;
     const unsavedButton = new UnsavedButton(wavesurfer, active, this.annotate);
 
-
+    //Once the webpage is changed, stop playing audio
     history.listen(() => {
       wavesurfer.stop();
     });
 
-    // spectrogram_created
+    //Event when wavesurfer finsihing rendering the spectrogram canvas
     wavesurfer.on('ready', () => {
+
+      //ADJUST SIZE OF SPECTROGRAM ON THE SCREEN
+      //TODO: CHECK WHY SOME DEVICES HAVE IT SO SMALL
       const screenSize = window.screen.width;
       if (screenSize > wavesurfer.getDuration() * wavesurfer.params.minPxPerSec) {
         wavesurfer.zoom(screenSize / wavesurfer.getDuration());
         wavesurfer.spectrogram._onUpdate(screenSize);
       }
+
+      //Allow people to drag regions with mouse click
       this.state.isRendering = false;
       this.setState({ isRendering: false });
       wavesurfer.enableDragSelection({ color: 'rgba(0, 102, 255, 0.3)' });
     });
+
+    //Event handler anytime the region is updated
+    //ie: moved, annotation label change etc
+    //Stops audio and sets button as unsaved
     wavesurfer.on('region-updated', region => {
       this.handlePause();
       this.styleRegionColor(region, 'rgba(0, 102, 255, 0.3)');
@@ -105,31 +151,52 @@ class WavesurferMethods {
       region._onUnSave();
     });
 
+    //Event handler for when a region is created
+    //Sets region as selectedSegment
     wavesurfer.on('region-created', region => {
+      //Pause playback
       this.handlePause();
+
+      //Set the new region created as the selected region
       const { storedAnnotations, applyPreviousAnnotations } = this.annotate.state;
       const {selectedSegment} = this.state
       if (selectedSegment) {
         selectedSegment._onSelect(false);
       }
+
+      //If we can carry over the last annotations, do so
       if (applyPreviousAnnotations) {
         region.data.annotations = storedAnnotations;
       }
+
+      //Add region to states
       this.setState({
         selectedSegment: region
       });
       unsavedButton.addUnsaved(region, !region.saved);
-      //region._onSelect(true)
     });
 
+    /**
+     * When spectrogram is rendered, save it to state for further transformations
+     * These transformations are enabled in extra features, see side menu and toggleable features
+     * for more info
+     */
     wavesurfer.on('spectrogram_created', spectrogram => {
       this.setState({ spectrogram });
     });
 
+    /**
+     * Handle envent when user click the spectrogram
+     * This currently pauses the audio
+     */
     wavesurfer.on('click', () => {
       this.handlePause();
     });
 
+    /**
+     * Handle event when user clicks on a region
+     * This sets the region as the selectedSegment
+     */
     wavesurfer.on('region-click', (r, e) => {
       const {selectedSegment} = this.state
       if (selectedSegment) {
@@ -143,6 +210,10 @@ class WavesurferMethods {
       r._onSelect(true);
     });
 
+    /**
+     * Handle event when user double clicks on a region
+     * TThis sets the region as selected and plays the audio the region contains
+     */
     wavesurfer.on('region-dblclick', (r, e) => {
       const {selectedSegment} = this.state
       if (selectedSegment) {
@@ -157,9 +228,16 @@ class WavesurferMethods {
       r._onSelect(true);
     });
 
+    /**
+     * Handle event for when audio is paused
+     */
     wavesurfer.on('pause', () => {
       this.setState({ isPlaying: false });
     });
+
+    /**
+     * Handle event for when audio is played
+     */
     wavesurfer.on('play', () => {
       this.setState({ isPlaying: true });
     });
@@ -168,28 +246,48 @@ class WavesurferMethods {
     return { wavesurfer, unsavedButton };
   }
 
+  /**
+   * Handler for when play button is pressed
+   * Starts audio
+   */
   handlePlay() {
     const { wavesurfer } = this.state;
     this.setState({ isPlaying: true });
     wavesurfer.play();
   }
 
+  /**
+   * Handler for when pause button is pressed
+   * Stops audio
+   */
   handlePause() {
     const { wavesurfer } = this.state;
     this.setState({ isPlaying: false });
     wavesurfer.pause();
   }
 
+  /**
+   * Handler for when forward button is pressed
+   * Moves 5 seconds ahead
+   */
   handleForward() {
     const { wavesurfer } = this.state;
     wavesurfer.skipForward(5);
   }
 
+  /**
+   * Handler for when backward button is pressed
+   * Moves 5 seconds back
+   */
   handleBackward() {
     const { wavesurfer } = this.state;
     wavesurfer.skipBackward(5);
   }
 
+  /**
+   * Handler for zoom value is changed
+   * Forces spectrogram to rerender to zoom value
+   */
   handleZoom(e) {
     const { wavesurfer } = this.state;
     const zoom = Number(e.target.value);
@@ -197,12 +295,18 @@ class WavesurferMethods {
     this.setState({ zoom });
   }
 
+  /**
+   * Recolors a given region with a given color
+   */
   styleRegionColor(region, color) {
     region.style(region.element, {
       backgroundColor: color
     });
   }
 
+  /**
+   * Renders pause/play backwards and forwards buttons
+   */
   renderButtons(isPlaying) {
     return (
       <div className="row justify-content-center my-4">
